6c2c635929b483c2da5764bf2cbd2129421f2a0f
LYZ <q751264263@vip.qq.com>
2018-03-26 22:58:48 +0800

    #widget test

---
+++ app/src/main/java/com/shikeclass/app/view/TimeView.java
@@ -0,0 +1,26 @@
+package com.shikeclass.app.view;
+
+import android.content.Context;
+import android.support.annotation.Nullable;
+import android.util.AttributeSet;
+import android.view.View;
+
+/**
+ * Created by LYZ on 2018/3/21 0021.
+ */
+
+public class TimeView extends View {
+    public TimeView(Context context) {
+        super(context);
+    }
+
+    public TimeView(Context context, @Nullable AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    public TimeView(Context context, @Nullable AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+    }
+
+
+}
---
+++ app/src/main/java/com/shikeclass/app/widget/TimeWidget.java
@@ -0,0 +1,10 @@
+package com.shikeclass.app.widget;
+
+import android.appwidget.AppWidgetProvider;
+
+/**
+ * Created by LYZ on 2018/3/21 0021.
+ */
+
+public class TimeWidget extends AppWidgetProvider {
+}
